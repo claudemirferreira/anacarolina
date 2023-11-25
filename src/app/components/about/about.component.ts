5cc236ae26e8d9e95d0e04153a33ffa035c959f0
Claudemir <claudemirramosferreira@gmail.com>
2023-11-25 14:38:18 -0300

    done education

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatCardModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
