f78ea53d2c7f0042e3a5db09f1677eebca547977
Claudemir <claudemirramosferreira@gmail.com>
2023-11-25 14:08:27 -0300

    done home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [CommonModule, MatIconModule,
     RouterLink, 
-    RouterOutlet ],
+    RouterOutlet,
+    MatToolbarModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
